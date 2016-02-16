d5e424b37f78c973bc1203cd30b3016988842665
ArioA <asa215@ic.ac.uk>
2016-02-16 09:19:11 +0000

    Made a start on the first three questions. Confident on first two but not on third. Need to test.

--- db_2016_cw3.sql
+++ db_2016_cw3.sql
@@ -1,13 +1,40 @@
 -- Q1 returns (first_name)
-
+SELECT SUBSTRING(person.name FROM 1 FOR POSITION(' ' IN person.name) -1 AS first_name
+FROM person
+ORDER BY first_name
 ;
 
 -- Q2 returns (born_in,popularity)
-
+SELECT person.born_in, COUNT(person.born_in) AS popularity
+FROM person
+GROUP BY person.born_in
+ORDER BY popularity
 ;
 
 -- Q3 returns (house,seventeenth,eighteenth,nineteenth,twentieth)
-
+SELECT monarch.house, COUNT(	SELECT monarch.house
+				FROM monarch
+				WHERE accession <= 1700-12-31
+				AND accession >= 1601-01-01
+				) AS seventeenth,
+COUNT(	SELECT monarch.house
+	FROM monarch
+	WHERE accession <= 1800-12-31
+	AND accession >= 1701-01-01
+	) AS eighteenth,
+COUNT(	SELECT monarch.house
+	FORM monarch
+	WHERE accession <= 1900-12-31
+	AND accession >= 1801-01-01
+	) AS nineteenth,
+COUNT(	SELECT monarch.house
+	FROM monarch
+	WHERE accession <= 2000-12-31
+	AND accession >= 1901-01-01
+	) AS twentieth
+FROM monarch
+GROUP BY house
+ORDER BY house
 ; 
 
 -- Q4 returns (name,age)
